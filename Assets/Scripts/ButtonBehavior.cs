edc13be04d997bd333214bee6fd66174bd22d804
AgnieszkaN97952 <97952@student.ue.poznan.pl>
2026-02-04 00:28:47 +0100

    Added killer choice screen

---
+++ Assets/Scripts/ButtonBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehavior : MonoBehaviour
+{
+    public void RyuClicked()
+    {
+        Debug.Log("Ryu is just blend");
+    }
+
+    public void SoraClicked()
+    {
+        Debug.Log("that's the killer");
+    }
+
+    public void AikoClicked()
+    {
+        Debug.Log("daughter couldn't kill");
+    }
+
+    public void MikoClicked()
+    {
+        Debug.Log("she just thought it was a joke");
+    }
+}
